9b69178bc15a4e27ffec6e6bce64ac82af808112
josephdaw <josephdaw@icloud.com>
2021-10-03 16:55:20 +1030

    add function to display time blocks in table

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,20 @@
 // display the current day
 var currentDay = moment().format("dddd, MMMM Do, YYYY");
 $("#currentDay").text(currentDay);
+
+// create array of work hours
+let workHours = [];
+// create hours from 9am to 5pm
+for (i=9; i<=17; i++){
+    workHours.push(i);
+};
+
+function displayTimeBlocks(){
+    for(i = 0; i < workHours.length; i++){
+        console.log(workHours[i]);
+        let momentHour = workHours[i];
+        let hour = moment(momentHour, "hh").format("h a");
+        console.log(hour);
+        $("#time-block-container").append(`<tr><td>${hour}</td></tr>`);
+    };
+};
