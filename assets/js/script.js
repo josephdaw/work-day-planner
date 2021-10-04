198b98de06bb2e54090ec52d26920e1d74aca41d
josephdaw <josephdaw@icloud.com>
2021-10-04 15:53:22 +1030

    save button click targets correct input box

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ $("#currentDay").text(currentDay);
 // create array of work hours
 let workHours = [];
 // create hours from 9am to 5pm
-for (i=9; i<=17; i++){
+for (i=8; i<=18; i++){
     workHours.push(i);
 };
 
@@ -16,9 +16,19 @@ function displayTimeBlocks(){
         let momentHour = workHours[i];
         let hour = moment(momentHour, "hh").format("h a");
         console.log(hour);
-        $("#time-block-container").append(`<tr><th scope="row">${hour}</th><td></td><td><button type="button" class="btn btn-success">Success</button></td></tr>`);
+        $("#time-block-container").append(`<tr><th scope="row">${hour}</th><td><input id="${workHours[i]}" type="text"></input></td><td><button type="button" class="btn btn-success btn-save">Save</button></td></tr>`);
         // $("#time-block-container").children().children().append(`<button type="button" class="btn btn-success save-btn">Save</button>`);
     };
+};
+
+// event listener on table - targeting click on 'save button'
+$('#time-block-container').on('click','.btn-save', getInput);
+
+function getInput(event){
+    // event.target.parent('tr').nearest('input').val()
+    let btnClicked = $(event.target)
+    let text = btnClicked.parent().parent().children().children('input').val().trim()
+    console.log(text);
 };
 
 function init(){
