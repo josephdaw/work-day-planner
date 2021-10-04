512cdae5d312fcc37e202d145ce0ba84e4c0fadd
josephdaw <josephdaw@icloud.com>
2021-10-04 16:28:25 +1030

    update workHours array with task information on 'save' click

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,17 +6,20 @@ $("#currentDay").text(currentDay);
 // create array of work hours
 let workHours = [];
 // create hours from 9am to 5pm
-for (i=8; i<=18; i++){
-    workHours.push(i);
+for (i=9; i<=17; i++){
+    workHours.push({
+        hour: i,
+        task: ""
+    });
 };
 
 function displayTimeBlocks(){
     for(i = 0; i < workHours.length; i++){
-        console.log(workHours[i]);
-        let momentHour = workHours[i];
+        // console.log(workHours[i].hour);
+        let momentHour = workHours[i].hour;
         let hour = moment(momentHour, "hh").format("h a");
-        console.log(hour);
-        $("#time-block-container").append(`<tr><th scope="row">${hour}</th><td><input id="${workHours[i]}" type="text"></input></td><td><button type="button" class="btn btn-success btn-save">Save</button></td></tr>`);
+        // console.log(hour);
+        $("#time-block-container").append(`<tr><th scope="row">${hour}</th><td><input id="${workHours[i].hour}" type="text"></input></td><td><button type="button" class="btn btn-success btn-save">Save</button></td></tr>`);
         // $("#time-block-container").children().children().append(`<button type="button" class="btn btn-success save-btn">Save</button>`);
     };
 };
@@ -25,10 +28,29 @@ function displayTimeBlocks(){
 $('#time-block-container').on('click','.btn-save', getInput);
 
 function getInput(event){
-    // event.target.parent('tr').nearest('input').val()
+    // find element targeted by click
     let btnClicked = $(event.target)
-    let text = btnClicked.parent().parent().children().children('input').val().trim()
+    // find closest user input field
+    let inputEl = btnClicked.parent().parent().children().children('input')
+    // grab user input
+    let text = inputEl.val().trim()
+    // grab input field id
+    let inputElId = inputEl.attr('id')
+
     console.log(text);
+    console.log(inputElId);
+    // iterate through workHours array and update 'task' information 
+    // for the corresponding id
+    workHours.forEach(id => {
+        if (id.hour == inputElId) 
+        id.task = text;
+    });
+    
+    console.log(workHours);
+
+    // save workHours in localStorage
+    
+
 };
 
 function init(){
